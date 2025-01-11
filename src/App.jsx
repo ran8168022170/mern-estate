c456c72dc90aa0af21e4b688e060f8bb0df5e9e9
Ranjeet Kanu <ranjeetkanu77@gmail.com>
2025-01-11 18:07:51 -0300

    Home defined in App and Home page  Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
