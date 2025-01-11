ecded0d075131d55544e229869f024bc39af10bd
Ranjeet Kanu <ranjeetkanu77@gmail.com>
2025-01-11 17:25:07 -0300

    Different route added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 export default function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
